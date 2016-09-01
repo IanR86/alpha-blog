29501f79ca7e3f2bd3906896e6b40ebd6ec792f5
IanR0 <ian.ross5@hotmail.co.uk>
2016-09-01 20:47:30 +0000

    Fix issue with duplicate column in migration files yet again

--- db/migrate/20160222211022_add_created_at_to_articles.rb
+++
@@ -1,5 +0,0 @@
-class AddCreatedAtToArticles < ActiveRecord::Migration
-  def change
-    add_column :articles, :timestamp
-  end
-end
--- db/migrate/20160222212644_add_updated_at_column.rb
+++
@@ -1,5 +0,0 @@
-class AddUpdatedAtColumn < ActiveRecord::Migration
-  def change
-    add_column :articles, :updated_at, :timestamp
-  end
-end
